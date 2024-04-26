82c1c18595314f4e5d85a39c8742309260a078d1
CassioSousa09 <148918527+CassioSousa09@users.noreply.github.com>
2024-04-26 19:48:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include "trabalho.h"
+#include <stdio.h>
 
+int main() {
+  int escolha;
+  char telefone[MAX_LENGTH];
+
+  while (1) {
+    printf("\nEscolha uma Opção:\n");
+    printf("1. Adicionar Contatos\n");
+    printf("2. Listar Contatos\n");
+    printf("3. Deletar Contatos\n");
+    printf("4. Salvar Agenda\n");
+    printf("5. Carregar Agenda\n");
+    printf("6. Sair\n");
+    scanf("%d", &escolha);
