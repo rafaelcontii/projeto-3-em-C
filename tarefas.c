e288786ae001b1d075c1c001a5a447bd9ab05382
rafaelcontii <148920793+rafaelcontii@users.noreply.github.com>
2024-04-26 20:10:58 -0300

    Update tarefas.c
    iniciando a função  'adicionar contatos'

--- tarefas.c
+++ tarefas.c
@@ -4,3 +4,15 @@
 
 Contato agenda[MAX_CONTACTS];
 int num_contatos = 0;
+
+void adicionar_contato() {
+  if (num_contatos < MAX_CONTACTS) {
+    Contato novo_contato;
+    printf("Digite o Nome: ");
+    scanf("%s", novo_contato.nome);
+    printf("Digite o Sobrenome: ");
+    scanf("%s", novo_contato.sobrenome);
+    printf("Digite o Email: ");
+    scanf("%s", novo_contato.email);
+    printf("Digite o Telefone: ");
+    scanf("%s", novo_contato.telefone);
