d9d4b9e9afc5cdabfa8487c9afd0ff9e2d77c45e
CassioSousa09 <148918527+CassioSousa09@users.noreply.github.com>
2024-04-26 19:49:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,3 +14,32 @@ int main() {
     printf("5. Carregar Agenda\n");
     printf("6. Sair\n");
     scanf("%d", &escolha);
+ switch (escolha) {
+    case 1:
+      adicionar_contato();
+      break;
+    case 2:
+      listar_contatos();
+      break;
+    case 3:
+      printf("Digite o telefone que deseja deletar: ");
+      scanf("%s", telefone);
+      deletar_contato(telefone);
+      break;
+    case 4:
+      salvar_agenda();
+      break;
+    case 5:
+      carregar_agenda();
+      break;
+    case 6:
+      printf("Aguarde, saindo..\n");
+      return 0;
+    default:
+      printf("Opção não encontrada!\n");
+    }
+  }
+
+  return 0;
+}
+//
